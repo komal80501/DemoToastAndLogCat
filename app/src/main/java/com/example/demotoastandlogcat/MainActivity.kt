77d896f407d0ca94f70c2ee23eab49251cc6268c
Hp <komalmane48@gmail.com>
2019-02-18 18:34:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/demotoastandlogcat/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.demotoastandlogcat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonToastShow.setOnClickListener {
+            //code
+            Log.i("MainActivity","Button Was Clicked!!!!")
+            Toast.makeText(this,"Button was Clicked",Toast.LENGTH_SHORT).show()
+
+        }
+    }
+}
